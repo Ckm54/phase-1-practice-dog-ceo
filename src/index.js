596b2662aa3dd2f407193b47afd68a26fda8d0bb
Collins <mutaic84@gmail.com>
2022-04-16 12:45:09 +0300

    add images to DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    let imageContainer = document.getElementById("dog-breeds")
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => {
+        data.message.forEach(imgUrl => {
+            const imageHolder = document.createElement("li");
+            const image = document.createElement("img")
+            image.setAttribute("src", imgUrl)
+            imageHolder.appendChild(image);
+            imageContainer.appendChild(imageHolder)
+        });
+    })
+})
+// function addImages()
